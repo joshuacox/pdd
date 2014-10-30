251c71efa3fa77af6efd38d96ecff13a41726f1e
Josh Cox <josh@webhosting.coop>
2014-10-30 09:21:49 -0500

    76 to 77, split shell provisioners to separate out vagrant stuff

--- packer/build.sh
+++ packer/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-rm -r vendor
-berks vendor vendor/cookbooks
-/usr/bin/time -v packer build -debug --only=virtualbox-iso pdd-debian-7.6-i386.json
-#/usr/bin/time -v packer build --only=virtualbox-iso pdd-debian-7.6-i386.json
+#rm -r vendor
+#rm Berksfile.lock
+#berks vendor vendor/cookbooks
+#/usr/bin/time -v packer build -debug --only=virtualbox-iso pdd-debian-7.7-i386.json
+#/usr/bin/time -v packer build --only=virtualbox-iso pdd-debian-7.7-i386.json
+# qemu only gives errors, because vagrant cant find the virtualbox image
+#/usr/bin/time -v packer build -debug --only=qemu pdd-debian-7.7-i386.json
+/usr/bin/time -v packer build --only=qemu pdd-debian-7.7-i386.json
+#/usr/bin/time -v packer build -parallel=false --only=qemu,virtualbox-iso pdd-debian-7.7-i386.json
+#/usr/bin/time -v packer build pdd-debian-7.7-i386.json
 # beeper
 aplay /usr/share/sounds/alsa/Front_Center.wav
 # ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
