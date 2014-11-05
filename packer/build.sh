db3643c903ac5f111f53445ad615c6983c7dca39
Josh Cox <josh@webhosting.coop>
2014-11-05 01:16:59 -0600

    guest additions working again

--- packer/build.sh
+++ packer/build.sh
@@ -3,10 +3,10 @@ rm -r vendor
 rm Berksfile.lock
 berks vendor vendor/cookbooks
 #/usr/bin/time -v packer build -debug --only=virtualbox-iso pdd-debian-7.7-i386.json
-#/usr/bin/time -v packer build --only=virtualbox-iso pdd-debian-7.7-i386.json
+/usr/bin/time -v packer build -debug --only=virtualbox-iso pdd-debian-7.7-i386.json
 # qemu only gives errors, because vagrant cant find the virtualbox image
 #/usr/bin/time -v packer build -debug --only=qemu pdd-debian-7.7-i386.json
-/usr/bin/time -v packer build --only=qemu pdd-debian-7.7-i386.json
+#/usr/bin/time -v packer build --only=qemu pdd-debian-7.7-i386.json
 #/usr/bin/time -v packer build -parallel=false --only=qemu,virtualbox-iso pdd-debian-7.7-i386.json
 #/usr/bin/time -v packer build pdd-debian-7.7-i386.json
 # beeper
